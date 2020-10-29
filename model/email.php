8136be382ceca50c8bd52b7bccb5acfcf02ed216
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-10-29 16:33:57 -0300

    añadida funcion generar codigo 6 digitos

--- model/email.php
+++ model/email.php
@@ -2,4 +2,11 @@
     function sendMail($destinatario,$asunto,$cuerpo){
         mail($destinatario,$asunto,$cuerpo,"From:portaldeeventos@us-imm-node5c.000webhost.io");
     }
+    function generarCodigo() {
+        $pattern = '1234567890abcdefghijklmnopqrstuvwxyz';
+        $max = strlen($pattern)-1;
+        $key = '';
+        for($i=0;$i < 6;$i++) $key .= $pattern[mt_rand(0,$max)];
+        return $key;
+    }
 ?>
