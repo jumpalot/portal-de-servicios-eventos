7e75e5ed313b170bc50240dbc06d3fbfe9d758d0
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-10-29 16:37:01 -0300

    codigo ahora en mayusculas

--- model/email.php
+++ model/email.php
@@ -3,7 +3,7 @@
         mail($destinatario,$asunto,$cuerpo,"From:portaldeeventos@us-imm-node5c.000webhost.io");
     }
     function generarCodigo() {
-        $pattern = '1234567890abcdefghijklmnopqrstuvwxyz';
+        $pattern = '1234567890ABCDEFGHIJKLOPQRSTUVWXYZ';
         $max = strlen($pattern)-1;
         $key = '';
         for($i=0;$i < 6;$i++) $key .= $pattern[mt_rand(0,$max)];
