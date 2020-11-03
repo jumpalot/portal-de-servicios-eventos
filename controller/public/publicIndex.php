d140f4030b08dd8b3f9913d7a18c50c141e60e2f
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-03 20:38:08 -0300

    legales añadidos, por completar

--- controller/public/publicIndex.php
+++ controller/public/publicIndex.php
@@ -3,6 +3,13 @@
     switch(@$_GET['section']){
         case 'idea':
             include('controller/public/ideas_detalle.php');
+        break;
+        case 'privacidad':
+            include('controller/public/legales/politica_privacidad.html');
+        break;
+        case 'terminos':
+            include('controller/public/legales/terminos_condiciones');
+        break;
         default:
             include('controller/public/home.php');
     }
