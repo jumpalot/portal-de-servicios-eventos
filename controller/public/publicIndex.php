60b6fa4a65b007d13fef8e16effff44923ca4074
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-03 20:41:25 -0300

    fix another typo

--- controller/public/publicIndex.php
+++ controller/public/publicIndex.php
@@ -5,10 +5,10 @@
             include('controller/public/ideas_detalle.php');
         break;
         case 'privacidad':
-            include('controller/public/legales/politica_privacidad.html');
+            include('view/public/legales/politica_privacidad.html');
         break;
         case 'terminos':
-            include('controller/public/legales/terminos_condiciones.html');
+            include('view/public/legales/terminos_condiciones.html');
         break;
         default:
             include('controller/public/home.php');
