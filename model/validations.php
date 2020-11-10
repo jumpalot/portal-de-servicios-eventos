e9bb1ec8207ceeb8a4d429561be4e7e3ec8fcbde
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-10 00:47:26 -0300

    ajustes a la longitud de los campos del usuario

--- model/validations.php
+++ model/validations.php
@@ -12,9 +12,9 @@
         return preg_match("/[0-9]{10,20}/", $tel);
     }
     function validmedia($fb, $tw, $ig){
-        return (!$tw|preg_match("/@([A-Za-z0-9_]{1,15})/", $tw)) &
-               (!$fb|preg_match("/([A-Za-z0-9_]{1,15})/", $fb))  &
-               (!$ig|preg_match("/@([A-Za-z0-9_]{1,15})/", $ig)) ;
+        return (!$tw|preg_match("/@([A-Za-z0-9_]{3,16})/", $tw)) &
+               (!$fb|preg_match("/([A-Za-z0-9_]{3,20})/", $fb))  &
+               (!$ig|preg_match("/@([A-Za-z0-9_]{3,16})/", $ig)) ;
     }
     function validweb($web){
         return preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $web);
