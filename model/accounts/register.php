1239a822b5f0055d04cc7186f9c5bd60847a1cdc
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-10 02:11:02 -0300

    corregido nombre incorrecto

--- model/accounts/register.php
+++ model/accounts/register.php
@@ -6,7 +6,7 @@
     if (@$_POST['code']){                                                       //si la pagina envio un codigo
         if(verificarCodigo($_POST['code'], $_POST['email'])){                   //lo verifico
             echo register(                                                      //devuelvo el id del nuevo usuario
-                $_POST['email'], $_POST['name'], $_POST['pass'], $_POST['tel'],
+                $_POST['email'], $_POST['name'], $_POST['code'], $_POST['tel'],
                @$_POST['fb'],   @$_POST['tw'],  @$_POST['ig'],  @$_POST['web']
             );
         } else echo 'noverif';                                                    //si no verifica lo aviso
