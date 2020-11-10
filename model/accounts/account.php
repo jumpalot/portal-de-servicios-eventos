5b3297097a42260012f460013445b5930d9586ee
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-10 02:29:55 -0300

    web ahora se añade al registro correctamente

--- model/accounts/account.php
+++ model/accounts/account.php
@@ -4,11 +4,15 @@
 
         $tel = preg_replace('/[^0-9]+/', '', $tel); //eliminar no numeros del nro de telefono
         $pass = md5($pass);                         //encriptar contraseña
-        $media = ($fb?"'$fb'":'null') .','. ($tw?"'$tw'":'null') .','. ($ig?"'$ig'":'null'); //valores opcionales ausentes se reemplazan por null
+        $opcionales =                               //valores opcionales ausentes se reemplazan por null
+            ($fb?"'$fb'":'null') .','.
+            ($tw?"'$tw'":'null') .','.
+            ($ig?"'$ig'":'null') .','.
+            ($web?"'$web'":'null'); 
 
         if (validemail($email)&validname($name)&validtel($tel)&validmedia($fb, $tw, $ig)&validweb($web)){ //validacion de datos
             $sql = "INSERT INTO `usuarios`(`nombre`, `pass`, `telefono`, `correo`, `fb`, `tw`, `ig`, `web`)
-                    VALUES ('$name','$pass','$tel','$email', $media)";
+                    VALUES ('$name','$pass','$tel','$email', $opcionales)";
             $db->query($sql);
             if($db->error=="") return $db->insert_id; //si la insercion no falló devuelve el nuevo id
         }
