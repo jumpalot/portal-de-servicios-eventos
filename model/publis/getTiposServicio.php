63b36cca055920c222275a8cec6c36791691985b
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-26 12:17:27 -0300

    webservice que faltaba

---
+++ model/publis/getTiposServicio.php
@@ -0,0 +1,9 @@
+<?php
+    include('../db.php');
+    $sql = "SELECT * FROM tiposervicio";
+    $servicios = $db->query($sql);
+    if ($db->error!="")
+        foreach ($servicios as $servicio):
+            echo $servicio[0].','.$servicio[1].';';
+        endforeach;
+?>
