94e50407fd7c39706a3d45fa9e3aa42cb6840018
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-26 09:00:33 -0300

    for escrito en formato of se habia confundico con as

--- js/users.js
+++ js/users.js
@@ -9,7 +9,7 @@ const listaTPublis = new Array(
 function mostrarNewPost2() {
     var tipo = document.getElementById("tipo");
     var opcion = tipo.options[tipo.selectedIndex];
-    for (listaTPublis of publi) document.getElementById(publi).style.display = "none";
+    for (publi of listaTPublis) document.getElementById(publi).style.display = "none";
     document.getElementById(opcion.value).style.display = "block";
 }
 function newPost(){
