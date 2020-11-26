3f0061585fff829841cf7e519f199e9f904a712e
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-26 14:45:23 -0300

    ahora las opciones del usuario cambian el puntero al correcto.

--- view/users/navuser.php
+++ view/users/navuser.php
@@ -4,11 +4,11 @@
         data-toggle="dropdown">
     </button>
     <div class="dropdown-menu" role="menu">
-        <a class="dropdown-item" data-toggle="modal" data-target="#profileModal">Perfil</a>
+        <a class="dropdown-item" data-toggle="modal" data-target="#profileModal" role="button">Perfil</a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" data-toggle="modal" data-target="#myPubsModal">Mis Publicaciones</a>
+        <a class="dropdown-item" data-toggle="modal" data-target="#myPubsModal" role="button">Mis Publicaciones</a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" data-toggle="modal" data-target="#newPostModal">Nueva publicacion</a>
+        <a class="dropdown-item" data-toggle="modal" data-target="#newPostModal" role="button">Nueva publicacion</a>
         <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="./?logout=1">Cerrar Sesion</a>
     </div>
