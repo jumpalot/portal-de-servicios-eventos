f8b861590e36fbe451e0bcc873ec12fddf60a12a
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-09 22:44:59 -0300

    lista archivos simplificada

--- model/publis/fotos/imgCache.php
+++ model/publis/fotos/imgCache.php
@@ -1,10 +1,6 @@
 <?php
     function getImgCache($dir){
-        $gestor_dir = opendir($dir);
-        $archivos = array();
-        while (false !== ($nombre_fichero = readdir($gestor_dir)))
-            array_push($archivos, $nombre_fichero);
-        return $archivos;
+        return scandir($dir);
     }
     function mvImgCache($archivos, $dir){
         foreach ($archivos as $archivo)
