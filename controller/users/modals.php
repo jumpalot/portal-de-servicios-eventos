e569d5094de2804e6c2d04f1f6542d13f3c36afb
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-13 04:15:19 -0300

    nuevos modals incluidos

--- controller/users/modals.php
+++ controller/users/modals.php
@@ -1,5 +1,7 @@
 <?php
     include 'view/users/result.html';
+    include 'view/users/confirmDelete.html';
+    include 'view/users/editPub.html';
     include 'view/users/profile.html';
     include 'controller/users/newPost.php';
     include 'view/users/mypubs.html';
