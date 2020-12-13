6e81e3f3473b84089e9f05e5c3ef9723cf2b3596
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-13 16:26:40 -0300

    missing session_start()

--- model/publis/rmPost.php
+++ model/publis/rmPost.php
@@ -1,7 +1,8 @@
 <?php
     include("../db.php");
+    session_start();
+    $idUsu = $_SESSION['usrId'];
     $idPub = $_POST['idPub'];
     $tipo = $_POST['tipo'];
-    $idUsu = $_SESSION['usrId'];
     rmPubli($tipo, $idPub, $idUsu);
 ?>
