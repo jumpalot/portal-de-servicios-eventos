270623f90c223ffdd1a32f20b531b09386e44221
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-16 14:21:09 -0300

    fix url modals

--- controller/users/modals.php
+++ controller/users/modals.php
@@ -3,7 +3,7 @@
     include './view/users/confirmDelete.html';
     include './view/users/editPub.html';
     include './view/users/profile.html';
-    include './view/users/mypubs.html';
+    include './view/users/myPubs.html';
     include './view/users/editFotos.html';
     include './view/users/modifyData.html';
     include './view/users/upgradePub.html';
