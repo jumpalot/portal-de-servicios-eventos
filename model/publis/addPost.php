604d1251a2615d6cc114793951ca40460e6faa51
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-17 01:40:44 -0300

    solo guardar imagenes si se subieron imagenes

--- model/publis/addPost.php
+++ model/publis/addPost.php
@@ -15,13 +15,15 @@
                 $tiposServicio = $_POST['tipoServicio'];
                 $idServicio = addServicio($nom, $desc, $zonaServicio, $tiposServicio, $idUsu);
                 if($idServicio!=null) {
-                    if($idFotoP = addServicioImgs($idServicio, $archivos)){
-                        setFotoP($tipo, $idPub, $idUsu, $idFotoP);
-                        mvImgCache($archivos, $dir, 'servicios');
-                    } else {
-                        echo '<h3 style="text-align:center"> La publicación se subió</h3><h3>Pero ocurrió un error al registrar sus fotos</h3><h3>Puede solucionarlo desdde Mis Publicaciones</h3>';
-                        rmImgCache($archivos, $dir);
-                        exit;
+                    if (count($archivos)>0){
+                        if($idFotoP = addServicioImgs($idServicio, $archivos)){
+                            setFotoP($tipo, $idPub, $idUsu, $idFotoP);
+                            mvImgCache($archivos, $dir, 'servicios');
+                        } else {
+                            echo '<h3 style="text-align:center"> La publicación se subió</h3><h3>Pero ocurrió un error al registrar sus fotos</h3><h3>Puede solucionarlo desdde Mis Publicaciones</h3>';
+                            rmImgCache($archivos, $dir);
+                            exit;
+                        }
                     }
                 } else {
                     echo '<h3 style="text-align:center"> No se pudo subir su publicacion</h3><h3>Error al cargar en la base de datos</h3>';
@@ -37,15 +39,17 @@
                 $servicios = $_POST['servicios'];
                 $idSalon = addSalon($nom, $desc, $cap, $zonasSalon, $tipoSalon, $idUsu);
                 if($idSalon!=null){
-                    if ($idFotoP = addSalonImgs($idSalon, $archivos)){
-                        addEspaciosSalon($espacios, $idSalon);
-                        addServiciosSalon($servicios, $idSalon);
-                        setFotoP($tipo, $idPub, $idUsu, $idFotoP);
-                        mvImgCache($archivos, $dir, 'salon');
-                    } else {
-                        echo '<h3 style="text-align:center"> La publicación se subió</h3><h3>Pero ocurrió un error al registrar sus fotos</h3><h3>Puede solucionarlo desdde Mis Publicaciones</h3>';
-                        rmImgCache($archivos, $dir);
-                        exit;
+                    if (count($archivos)>0){
+                        if ($idFotoP = addSalonImgs($idSalon, $archivos)){
+                            addEspaciosSalon($espacios, $idSalon);
+                            addServiciosSalon($servicios, $idSalon);
+                            setFotoP($tipo, $idPub, $idUsu, $idFotoP);
+                            mvImgCache($archivos, $dir, 'salon');
+                        } else {
+                            echo '<h3 style="text-align:center"> La publicación se subió</h3><h3>Pero ocurrió un error al registrar sus fotos</h3><h3>Puede solucionarlo desdde Mis Publicaciones</h3>';
+                            rmImgCache($archivos, $dir);
+                            exit;
+                        }
                     }
                 } else {
                     echo '<h3 style="text-align:center"> No se pudo subir su publicacion.</h3><h3>Error al cargar en la base de datos</h3>';
