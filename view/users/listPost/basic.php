b8c093eb5da030821efff833a9db8b91a10e9ecd
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-17 13:19:31 -0300

    revertir cambios a pub basic

--- view/users/listPost/basic.php
+++ view/users/listPost/basic.php
@@ -1,12 +1,9 @@
 <div class="productos br input-contenedor-basic" id="<?=$tipo?>-<?=$pubId?>" onclick="editarPub(this.id)">
     <div class="cont2 pd10">
-        <h5 class="inline" id="titulo"><?=$titulo?></h5>
-        &nbsp;&nbsp;&nbsp;
-        <p class="cuerpo2 inline">
+        <h5 id="titulo"><?=$titulo?></h5>
+        <p class="cuerpo2">
             <i class="fas fa-map-marker-alt"></i>
-            &nbsp;
             <?=$zona?>
-            &nbsp;
         </p>
         <p class="cuerpo1 lnh">
             <?=$desc?>
