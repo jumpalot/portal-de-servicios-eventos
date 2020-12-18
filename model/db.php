c91702fc091621ccddb5535eb7b3ed73fc6e86c7
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-18 15:44:28 -0300

    query rmFotos arreglada

--- model/db.php
+++ model/db.php
@@ -152,12 +152,13 @@
     function rmFotosPubli($tipo, $idPub, $idUsu, $fotos){
         global $db;
         $uctipo = ucfirst($tipo);
-        $sql = "DELETE FROM fotos$tipo AS fp
+        $sql = "DELETE fp
+                FROM fotos$uctipo AS fp
                 NATURAL JOIN $tipo AS tp
                 WHERE fp.id_$tipo=$idPub AND tp.id_usuario=$idUsu AND (";
         foreach ($fotos as $foto)
             $sql .= "id_fotos=$foto OR ";
-        $sql = substr($sql, 0, -4);
+        $sql = substr($sql, 0, -4)+')';
         $db->query($sql);
         if ($db->error) {
             echo '<script>console.log("'.$db->error.'");</script>';
