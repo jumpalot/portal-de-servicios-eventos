dd233756f889e1a6b8b9fff7d4eeb1b7b92a29af
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-17 23:42:42 -0300

    bordes eliminados de itemEdit

--- view/users/itemEditFoto.php
+++ view/users/itemEditFoto.php
@@ -1,12 +1,12 @@
 <div class="input-group mb-3">
-  <div class="input-group-prepend border border-light h-50">
-    <span class="input-group-text w-50">
+  <div class="input-group-prepend h-50">
+    <span class="input-group-text border-0 h-50 w-50">
         <input type="radio" name="fotoP" class="radioFotoP" value="<?=$idFoto?>" <?=$checked?>>
     </span>
   </div>
-  <img src="<?=$foto?>" class="border border-light h-50">
-  <input type="text" class="form-control border border-light h-50" value="<?=$nombre?>" readonly>
-  <div class="input-group-append border border-light h-50">
-    <span class="input-group-text"><i class="fas fa-trash"></i></span>
+  <img src="<?=$foto?>" class="border-0 h-50">
+  <input type="text" class="form-control border-0 h-50" value="<?=$nombre?>" readonly>
+  <div class="input-group-append h-50">
+    <span class="input-group-text border-0 h-50"><i class="fas fa-trash"></i></span>
   </div>
 </div>
