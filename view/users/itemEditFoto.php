e6d6b1399fb842824a8dd7aabe4c867ccb3e0399
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-17 23:35:56 -0300

    estilo lista fotos

--- view/users/itemEditFoto.php
+++ view/users/itemEditFoto.php
@@ -1,12 +1,12 @@
 <div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text">
+  <div class="input-group-prepend border border-light h-50">
+    <span class="input-group-text w-50">
         <input type="radio" name="fotoP" class="radioFotoP" value="<?=$idFoto?>" <?=$checked?>>
     </span>
   </div>
-  <img src="<?=$foto?>">
-  <input type="text" class="form-control" value="<?=$nombre?>" readonly>
-  <div class="input-group-append">
+  <img src="<?=$foto?>" class="border border-light h-50">
+  <input type="text" class="form-control border border-light h-50" value="<?=$nombre?>" readonly>
+  <div class="input-group-append border border-light h-50">
     <span class="input-group-text"><i class="fas fa-trash"></i></span>
   </div>
 </div>
