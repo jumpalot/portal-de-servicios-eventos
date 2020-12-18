191b97d9750401f9dcd94503135dde8877cc60b9
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-17 22:44:20 -0300

    orden pub basic igual que en pub pro

--- view/users/listPost/basic.php
+++ view/users/listPost/basic.php
@@ -1,10 +1,10 @@
 <div class="productos br input-contenedor-basic" id="<?=$tipo?>-<?=$pubId?>" onclick="editarPub(this.id)">
     <div class="cont2 pd10">
-        <h5 id="titulo"><?=$titulo?></h5>
         <p class="cuerpo2">
             <i class="fas fa-map-marker-alt"></i>
             <?=$zona?>
         </p>
+        <h5 id="titulo"><?=$titulo?></h5>
         <p class="cuerpo1 lnh">
             <?=$desc?>
         </p>
