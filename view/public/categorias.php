dfe325bd3d02bfc57d0922ddbb0df7432dc16fbb
frank <frankxjx8@hotmail.com>
2020-12-19 19:52:25 -0300

    categorias

--- view/public/categorias.php
+++ view/public/categorias.php
@@ -1,14 +1,20 @@
 <?php
-    $tps = getTiposervicios();
+    include ("././model/db.php");
+    $tps = getTiposerviios();
 ?>
 <a class="nav-link" href="#" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">Servicios </a>
 <div class="dropdown">
-  <!-- <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-    Categorias
-  </button> -->
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <?php while($tp = $tps->fetch_object()): ?>
-    <li><a class="dropdown-item" href="?section=search&tipo=<?=$tp->nombre?>"><?=$tp->nombre?></a></li>
-    <?php endwhile; ?>
-  </ul>
+<button class="dropbtn">categorias
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+        <div class="row">
+          <div class="column">
+           <?php while($tp = $tps->fetch_object()): ?>
+            <li><a href="?section=search&tipo=<?=$tp->nombre?>"><?=$tp->nombre?></a></li>
+           <?php endwhile; ?>
+          </div>
+        </div>
+    </div>
+
 </div>
