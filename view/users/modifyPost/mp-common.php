6f12267ed9b6b6545f5358d6f1816fc40b1e4bee
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-19 19:31:11 -0300

    reducir un poco tamaño textarea

--- view/users/modifyPost/mp-common.php
+++ view/users/modifyPost/mp-common.php
@@ -2,5 +2,5 @@
         <input type="text" placeholder="Titulo" name="nom" pattern="[a-zA-Z0-9]{5,20}" value="<?=$titulo?>" required> 
     </div>
     <div class="input-contenedor">
-        <textarea name="desc" cols="60" rows="10" class="form-control" placeholder="Descripción" required><?=$desc?></textarea>
+        <textarea name="desc" cols="60" rows="9" class="form-control" placeholder="Descripción" required><?=$desc?></textarea>
     </div>
