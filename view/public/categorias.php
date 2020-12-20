1c580c694a031edb2540fc0bae9b1eed97275584
frank <frankxjx8@hotmail.com>
2020-12-20 18:12:17 -0300

    serv

--- view/public/categorias.php
+++ view/public/categorias.php
@@ -2,7 +2,7 @@
     $tps = getTiposervicios();
 ?>
 <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
           Servicios
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
