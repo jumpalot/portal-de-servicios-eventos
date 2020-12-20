8f12f2257b0a51fede1478cf8d84d9f925acf36e
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-20 16:58:00 -0300

    cambiar orden body

--- admin/controller/body.php
+++ admin/controller/body.php
@@ -2,14 +2,14 @@
 
     <?php
         include './view/body/setZonas.html';
-        include './view/body/rmZonas.html';
         include './view/body/setTipoServicios.html';
-        include './view/body/rmTipoServicios.html';
         include './view/body/setTipoSalon.html';
+        include './view/body/rmZonas.html';
+        include './view/body/rmTipoServicios.html';
         include './view/body/rmTipoSalon.html';
+        include './view/body/rmUsuarios.html';
         include './view/body/rmServicios.html';
         include './view/body/rmSalon.html';
-        include './view/body/rmUsuarios.html';
     ?>
 
 </div>
