2cc9ab43cf944d45e9ae4da76b7c13bbbe0ed3d3
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-20 14:20:56 -0300

    fin debug account

--- model/accounts/account.php
+++ model/accounts/account.php
@@ -14,8 +14,7 @@
             $sql = "INSERT INTO `usuarios`(`nombre`, `pass`, `telefono`, `correo`, `fb`, `tw`, `ig`, `web`)
                     VALUES ('$name','$pass','$tel','$email', $opcionales)";
             $db->query($sql);
-            echo $sql;
-            if($db->error=="") return $db->insert_id; //si la insercion no falló devuelve el nuevo id
+            if($db->error=="") return ""; //si la insercion no falló devuelve el nuevo id
         }
         return 'noreg';
     }
