43fafad02d97441e595ee9c47e63238f36416df9
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-21 00:34:00 -0300

    eliminar imagenes del usuario al eliminarlo

--- admin/model/rmUsuarios.php
+++ admin/model/rmUsuarios.php
@@ -2,9 +2,14 @@
     session_start();
     if (@$_SESSION['adminlogin']){
         include './db.php';
+        include '../../model/publis/fotos/imgCache.php';
         $email = $_POST['email'];
-        if (rmUsuario($email))
+        $idUsu = getIdUsu($email);
+        if (rmUsuario($email)){
             echo 'Se elimino correctamente el usuario';
+            if ($idUsu==null || !rmTree("../../img/$idUsu"))
+                echo '. Pero no pudieron eliminarse sus imagenes';
+        }
         else
             echo 'Ocurrio un error al eliminar el usuario';
     }
--- model/publis/fotos/imgCache.php
+++ model/publis/fotos/imgCache.php
@@ -19,4 +19,12 @@
             echo 'Imagen Borrada: '.$dir.$archivo['foto'];
         }
     }
+    function rmTree($dir) {
+        $files = array_diff(scandir($dir), array('.','..'));
+        foreach ($files as $file)
+            if (is_dir("$dir/$file")) 
+                 rmTree("$dir/$file");
+            else unlink("$dir/$file");
+        return rmdir($dir);
+    }
 ?>
