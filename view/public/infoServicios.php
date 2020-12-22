b0762a4f084c5f131d0a71b47a8ed6b339c9b4fe
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-21 23:13:03 -0300

    correccion etiqueta sin cerrar

--- view/public/infoServicios.php
+++ view/public/infoServicios.php
@@ -1,10 +1,12 @@
 <div class="infoServicios" id="wrapper-ipub">
-    <div class="wpr wpr-1">
-        <h5 class="wp-tipo">Sobre el lugar</h5>
-    </div>
-    <div class="wpr wpr-2">
-        <h6 class="wp-tipo">zona</h6>      <div class="wp-dato"><?=$zona?></div>
-    </div>
-    <div class="wpr wpr-3">
-        <h6 class="wp-tipo">categoría</h6> <div class="wp-dato"><?=$subtipo?></div>
+    <div id="wrapper-ipub">
+        <div class="wpr wpr-1">
+            <h5 class="wp-tipo">Sobre el lugar</h5>
+        </div>
+        <div class="wpr wpr-2">
+            <h6 class="wp-tipo">zona</h6>      <div class="wp-dato"><?=$zona?></div>
+        </div>
+        <div class="wpr wpr-3">
+            <h6 class="wp-tipo">categoría</h6> <div class="wp-dato"><?=$subtipo?></div>
+        </div>
     </div>
