0a6a4e3dcead1d852677d97bbbc5b4073a52af0f
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 01:31:56 -0300

    agregar link a regresar

--- view/public/banners/detalle.php
+++ view/public/banners/detalle.php
@@ -1,6 +1,7 @@
 <section class="cvl detalle" id="wp-ban">
-    <img src="./svg/banderin-seccion.svg" class="regresar">
+    <a href="?section=<?=$tipo?>">
+        <img src="./svg/banderin-seccion.svg" class="regresar">
         <h2 class="regresar">&nbsp;&#5176;&nbsp; <?=$seccion?></h2>
-    </img>
+    </a>
     <img class="crop-333 col-12" src="./img/<?=$idUsu?>/<?=$tipo?>/<?=$banner?>"/>
 </section>
