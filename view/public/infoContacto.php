b16dde5196052baad0976defb9beb118e2f10ca8
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 02:27:56 -0300

    abrir enlaces en nuevas pestañas

--- view/public/infoContacto.php
+++ view/public/infoContacto.php
@@ -10,10 +10,10 @@
         <h6>&nbsp; &nbsp;<?=$email?></h6>
     </div>
     <div id="wp-reds">
-        <?php if($ig): ?> <a href="https://www.instagram.com/<?=$ig?>"><i class="fab fa-instagram"></i></a> <?php endif; ?>
-        <?php if($tw): ?> <a href="https://twitter.com/<?=$ig?>"><i class="fab fa-twitter"></i></a> <?php endif; ?>
-        <?php if($fb): ?> <a href="https://www.facebook.com/<?=$fb?>"><i class="fab fa-facebook"></i></a> <?php endif; ?>
-        <?php if($web): ?> <a href="<?=$web?>"><i class="fas fa-globe"></i></a> <?php endif; ?>
+        <?php if($ig): ?> <a href="https://www.instagram.com/<?=$ig?>" target="_blank"><i class="fab fa-instagram"></i></a> <?php endif; ?>
+        <?php if($tw): ?> <a href="https://twitter.com/<?=$ig?>" target="_blank"><i class="fab fa-twitter"></i></a> <?php endif; ?>
+        <?php if($fb): ?> <a href="https://www.facebook.com/<?=$fb?>" target="_blank"><i class="fab fa-facebook"></i></a> <?php endif; ?>
+        <?php if($web): ?> <a href="<?=$web?>" target="_blank"><i class="fas fa-globe"></i></a> <?php endif; ?>
     </div>
 </div>
 </div>
