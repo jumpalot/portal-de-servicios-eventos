d65d352ad9b473bdec6f4c022d93f1b0510b8d39
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 19:12:37 -0300

    añadir detalle salon

---
+++ controller/public/salon_detalle.php
@@ -0,0 +1,76 @@
+<?php
+    function listar($datos){
+        $ldatos = '';
+        while ($dato = $datos->fetch_object())
+        $ldatos .= $dato->nombre.' / ';
+        return substr($ldatos, 0, -2);
+    }
+
+    $idPub = $_GET['idPub'];
+    $tipo = $_GET['tipo'];
+
+    $datos = getSalon($idPub);
+    $espaciosdb = getEspaciosPub($datos->id);
+    $lserviciosdb = getSalonlServicio($datos->id);
+
+    $idUsu = $datos->idUsu;
+
+    //titulo
+    $seccion = 'Salon';
+    $titulo = $datos->titulo;
+    echo "<h5 id=\"breadcrumb\">HOME / $seccion / $titulo </h5>";
+    echo '<section id="wrapper-detalle">';
+    
+    //banner
+    $banner = $datos->foto;
+    include('./view/public/banners/detalle.php');
+
+    //body
+    $body = $datos->descripcion;
+    $volanta =($datos->descuento!="0")?$$datos->descuento.'% OFF':"";
+    include('./view/public/ideas/cuerpo.php');
+    
+    //infoPub
+    $zona = $datos->zona;
+    $subtipo= $datos->tipo;
+    $capacidad = $datos->capacidad;
+    $espacios = listar($espaciosdb);
+    $servicios = listar($lserviciosdb);
+    include('./view/public/infoSalon.php');
+    
+    //carousel
+    include('./view/public/carousel/carouselTop.html');
+    $fotos = getFotosPubli('salon', $idPub, $idUsu);
+    $img = array();
+    $cont=0;
+    $contSecciones=1;
+    $active="active";
+    foreach($fotos as $foto){
+        if (++$cont==4){
+            include('./view/public/carousel/ideaItem.php');
+            if($active!="") $active="";
+            $cont=0;
+            $img = array();
+            $contSecciones++;
+        }
+        $img[$cont] = $foto['foto'];
+    }
+    if(isset($img[1])) 
+        include('./view/public/carousel/ideaItem.php');
+    include('./view/public/carousel/carouselEnd.html');
+    
+    //infocontacto
+    $nombreUsu = $datos->nombreUsu;
+    $telefono = '+'.((strlen($datos->telefono)>10)? $datos->telefono : '54'.$datos->telefono);
+    $email = $datos->email;
+    $fb = ($datos->fb=="NULL")? null : $datos->fb;
+    $tw = ($datos->tw=="NULL")? null : substr($datos->tw, 1);
+    $ig = ($datos->ig=="NULL")? null : substr($datos->ig, 1);
+    $web = ($datos->web=="NULL")? null : $datos->web;
+    include('./view/public/infoContacto.php');
+    
+    //contacto
+    include('./view/public/columContacto.html');
+
+    echo '</section>';
+?>    
---
+++ view/public/infoSalon.php
@@ -0,0 +1,9 @@
+<div id="wrapper-inf" class="infoServicios border">
+    <div id="wrapper-ipub">
+            <h3 class="wpr wpr-1 wp-tipo bold">Sobre el lugar</h3>
+            <h6 class="wpr wpr-2 wp-tipo">zona</h6>             <div class="wpr wpr-2 wp-dato"><?=$zona?></div>
+            <h6 class="wpr wpr-3 wp-tipo">categoría</h6>        <div class="wpr wpr-3 wp-dato"><?=$subtipo?></div>
+            <h6 class="wpr wpr-3 wp-tipo">capacidad</h6>        <div class="wpr wpr-3 wp-dato"><?=$capacidad?></div>
+            <h6 class="wpr wpr-3 wp-tipo">espacios</h6>        <div class="wpr wpr-3 wp-dato"><?=$espacios?></div>
+            <h6 class="wpr wpr-3 wp-tipo">servicios</h6>        <div class="wpr wpr-3 wp-dato"><?=$servicios?></div>
+    </div>
