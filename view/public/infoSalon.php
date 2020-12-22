8f0ded8315086c912a01787f9e95b35da6db06ee
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 19:16:40 -0300

    centrar titulo ipub

--- view/public/infoSalon.php
+++ view/public/infoSalon.php
@@ -1,6 +1,6 @@
 <div id="wrapper-inf" class="infoServicios border">
     <div id="wrapper-ipub">
-            <h3 class="wpr wpr-1 wp-tipo bold">Sobre el lugar</h3>
+            <h3 class="wpr wpr-1 wp-tipo-dato bold">Sobre el lugar</h3>
             <h6 class="wpr wpr-2 wp-tipo">zona</h6>             <div class="wpr wpr-2 wp-dato"><?=$zona?></div>
             <h6 class="wpr wpr-3 wp-tipo">categoría</h6>        <div class="wpr wpr-3 wp-dato"><?=$subtipo?></div>
             <h6 class="wpr wpr-3 wp-tipo">capacidad</h6>        <div class="wpr wpr-3 wp-dato"><?=$capacidad?></div>
