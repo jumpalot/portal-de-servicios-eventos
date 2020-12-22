e586f4348bd3c685582c1c0a5071eaa1090c6ed7
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 02:13:22 -0300

    titulo mas grande infoservicios

--- view/public/infoServicios.php
+++ view/public/infoServicios.php
@@ -1,6 +1,6 @@
 <div id="wrapper-inf" class="infoServicios border">
     <div id="wrapper-ipub">
-            <h5 class="wpr wpr-1 wp-tipo">Sobre el lugar</h5>
+            <h3 class="wpr wpr-1 wp-tipo bold">Sobre el lugar</h3>
             <h6 class="wpr wpr-2 wp-tipo">zona</h6>             <div class="wpr wpr-2 wp-dato"><?=$zona?></div>
             <h6 class="wpr wpr-3 wp-tipo">categoría</h6>        <div class="wpr wpr-3 wp-dato"><?=$subtipo?></div>
     </div>
