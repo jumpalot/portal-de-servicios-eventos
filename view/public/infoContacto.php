0476851051e2e48e03147ddd109bdca84c76231d
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 02:56:10 -0300

    telefono y whatsapp funcional

--- view/public/infoContacto.php
+++ view/public/infoContacto.php
@@ -1,14 +1,14 @@
 <div class="infoContacto" id="wrapper-iusu">
     <h5 id="wp-titu">Contacto Directo</h5>
     <h6 id="wp-nomb"><?=$nombreUsu?></h6>
-    <div id="wp-celu">
-        <i class="fas fa-phone-alt"></i>
+    <a id="wp-celu" href="https://wa.me/<?=$telefono?>" target="_blank">
+        <i class="fab fa-whatsapp"></i>
         <h6>&nbsp; &nbsp;<?=$telefono?></h6>
-    </div>
-    <div id="wp-mail">
+    </a>
+    <a id="wp-mail" href="mailto:<?=$email?>">
         <i class="fas fa-envelope"></i>
         <h6>&nbsp; &nbsp;<?=$email?></h6>
-    </div>
+    </a>
     <div id="wp-reds">
         <?php if($ig): ?> <a href="https://www.instagram.com/<?=$ig?>" target="_blank"><i class="fab fa-instagram"></i></a> <?php endif; ?>
         <?php if($tw): ?> <a href="https://twitter.com/<?=$ig?>" target="_blank"><i class="fab fa-twitter"></i></a> <?php endif; ?>
