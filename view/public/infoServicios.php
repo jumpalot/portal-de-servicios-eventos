1d0957489a4a155d8e73af40db3c651f4d71ed3c
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 01:20:23 -0300

    borde a infoServicios

--- view/public/infoServicios.php
+++ view/public/infoServicios.php
@@ -1,4 +1,4 @@
-<div class="infoServicios">
+<div class="infoServicios border">
     <div id="wrapper-ipub">
             <h5 class="wpr wpr-1 wp-tipo">Sobre el lugar</h5>
             <h6 class="wpr wpr-2 wp-tipo">zona</h6>             <div class="wpr wpr-2 wp-dato"><?=$zona?></div>
