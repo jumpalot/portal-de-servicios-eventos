fb5bdfe741bb33b1a9f0f6bec57f5423de2f9137
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 01:56:25 -0300

    descripcion en fila separada

--- view/public/ideas/cuerpo.php
+++ view/public/ideas/cuerpo.php
@@ -1,6 +1,5 @@
 <div id="wrapper-bod">
-    <div id="wp-desc">
-        <h4 class="toro fit"><?=$volanta?></h4>
-        <h2 class="toro bold"><?=$titulo?></h2>
-        <?=$body?>
-    </div>
+    <h4 class="toro fit"><?=$volanta?></h4>
+    <h2 class="toro bold"><?=$titulo?></h2>
+    <?=$body?>
+</div>
