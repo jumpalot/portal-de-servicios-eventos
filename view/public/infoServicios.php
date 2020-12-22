6d01f3c7d2765f649a76cc9a479a5392cc03a6d5
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 00:29:15 -0300

    columnas modificadas

--- view/public/infoServicios.php
+++ view/public/infoServicios.php
@@ -1,12 +1,6 @@
 <div class="infoServicios">
     <div id="wrapper-ipub">
-        <div class="wpr wpr-1">
-            <h5 class="wp-tipo">Sobre el lugar</h5>
-        </div>
-        <div class="wpr wpr-2">
-            <h6 class="wp-tipo">zona</h6>      <div class="wp-dato"><?=$zona?></div>
-        </div>
-        <div class="wpr wpr-3">
-            <h6 class="wp-tipo">categoría</h6> <div class="wp-dato"><?=$subtipo?></div>
-        </div>
+            <h5 class="wpr wpr-1 wp-tipo">Sobre el lugar</h5>
+            <h6 class="wpr wpr-2 wp-tipo">zona</h6>             <div class="wpr wpr-2 wp-dato"><?=$zona?></div>
+            <h6 class="wpr wpr-3 wp-tipo">categoría</h6>        <div class="wpr wpr-3 wp-dato"><?=$subtipo?></div>
     </div>
