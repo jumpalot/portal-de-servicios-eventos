89f164ecfaa7e3aa4bbdffadb3052929cdb70cf8
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 04:11:24 -0300

    propiedad inutil eliminada

--- view/public/categorias/categoria.php
+++ view/public/categorias/categoria.php
@@ -4,7 +4,6 @@
             src="<?=$foto?>"
             alt="./img/proximamente.png"
             class="card-img-salon"
-            height="300"
         />
         <div class="card-img-overlay infocards">
         <p class="cardfonts"><?=$nombre?></p>
