513f1faf0e93973bd5cf3a09547f1cb54d73ba46
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 01:22:30 -0300

    cosas de home ordenadas

--- controller/public/home.php
+++ controller/public/home.php
@@ -1,15 +1,15 @@
 <?php
     #mostrar firstSearch
-    include('./view/public/firstSearch.html');
+    include('./view/public/home/firstSearch.html');
 
     #variables carousel
     # $img1=...
     #mostrar carousel
-    include('./view/public/carouselHome.html');
+    include('./view/public/carousel/carouselHome.html');
 
     #mostrar banner
     include('./view/public/banners/home.html');
 
     #mostrar quienes somos
-    include('./view/public/quienesSomos.html');
+    include('./view/public/home/quienesSomos.html');
 ?>
