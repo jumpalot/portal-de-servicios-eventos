a16d514e503aaff416b07c5d1a7130e57052e18c
frank <frankxjx8@hotmail.com>
2020-12-23 16:21:16 -0300

    foto

--- view/public/busqueda/items/basic.php
+++ view/public/busqueda/items/basic.php
@@ -22,7 +22,7 @@
         <input type="submit" value="&#10009;" class="bbrrr pd10 boton">
     </div>
     <div class="cont1 pd10">
-        <img src="./img/<?=$idUsu?>/<?=$tipo?>/<?=$foto?>" width="200" height="200">
+        <img src="./img/<?=$foto?>" width="200" height="200">
     </div>
 </div> 
 <?php endwhile; ?>
