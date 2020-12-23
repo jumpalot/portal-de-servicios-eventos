75c341eec83c70b48a2436777e740087b1671d80
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 15:18:38 -0300

    rows promociones

--- controller/public/promociones.php
+++ controller/public/promociones.php
@@ -1,6 +1,13 @@
 <?php
     $pagina = $_POST['pagina'];
 
+    echo '<div class="row row-cols-1 row-cols-md-2">';
     include '../../view/public/promociones/promo.php';
+    include '../../view/public/promociones/promo.php';
+    echo '</div>';
     
+    echo '<div class="row row-cols-1 row-cols-md-2">';
+    include '../../view/public/promociones/promo.php';
+    include '../../view/public/promociones/promo.php';
+    echo '</div>';
 ?>
