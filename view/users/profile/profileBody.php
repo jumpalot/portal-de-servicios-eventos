53bdf5e144d97b5ff1d3f6e4c6090c27c1ff36ed
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 00:08:31 -0300

    modals perfil

---
+++ view/users/profile/profileBody.php
@@ -0,0 +1,29 @@
+<div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img src="https://static.vecteezy.com/system/resources/previews/000/550/731/non_2x/user-icon-vector.jpg" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title text-center"><?=$nombre?></h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Dni:44094767</li>
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <p class="text-center">
+                    <a href="https://www.instagram.com/emirko.07/?hl=es-la">
+                        <img src="https://icons.iconarchive.com/icons/icons8/ios7/48/Logos-Instagram-icon.png"> 
+                    </a>
+                    <a href="">
+                        <img src="https://icons.iconarchive.com/icons/danleech/simple/48/facebook-icon.png">
+                    </a> 
+                    <a href="">
+                        <img src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/48/Twitter-icon.png">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
