eeefc61e66e141d28b7075298318092d337ce1fd
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 21:52:38 -0300

    ahora todos los campos del presupuesto son requeridos

--- view/public/columContacto.php
+++ view/public/columContacto.php
@@ -3,30 +3,30 @@
     <h5>Datos del evento</h5>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-calendar-alt"></i></div>
-        <input class="form-control" placeholder="Fecha del Evento" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" name="fecha" />
+        <input class="form-control" placeholder="Fecha del Evento" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" name="fecha" required />
     </div>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-users"></i></div>
-        <input class="form-control" placeholder="Cantidad de invitados" type="text" name="cantidad" />
+        <input class="form-control" placeholder="Cantidad de invitados" type="text" name="cantidad" required />
     </div>
     <h5>Datos del contacto</h5>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-user"></i></div>
-        <input class="form-control" placeholder="Nombre" type="text" name="nombre" />
+        <input class="form-control" placeholder="Nombre" type="text" name="nombre" required />
     </div>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-user"></i></div>
-        <input class="form-control" placeholder="Apellido" type="text" name="apellido" />
+        <input class="form-control" placeholder="Apellido" type="text" name="apellido" required />
     </div>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-phone-alt"></i></i></div>
-        <input class="form-control" placeholder="Telefono" type="tel" name="telefono" />
+        <input class="form-control" placeholder="Telefono" type="tel" name="telefono" required />
     </div>
     <div class="input-group">
         <div class="input-group-prepend"><i class="fas fa-envelope"></i></div>
-        <input class="form-control" placeholder="Direccion de correo" type="email" name="email" />
+        <input class="form-control" placeholder="Direccion de correo" type="email" name="email" required />
     </div>
-    <textarea class="form-control border-0" name="comentarios" cols="30" rows="7" placeholder="Comentarios o sugerencias"></textarea>
+    <textarea class="form-control border-0" name="comentarios" cols="30" rows="7" placeholder="Comentarios o sugerencias" required></textarea>
     <input type="hidden" name="titulo" value="<?=$titulo?>">
     <input type="hidden" name="destinatario" value="<?=$email?>">
     <input type="submit" class="bblr border-0">
