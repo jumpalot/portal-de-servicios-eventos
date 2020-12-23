2cdc580f5a4a488279d8ff703d4be1dec4a9dd26
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 15:05:24 -0300

    boton cargar mas

--- js/promos.js
+++ js/promos.js
@@ -1,9 +1,18 @@
+var pagina = 1;
 $(document).ready( () => {
+    cargarPromos();
+    $('#cargarMas').on('click', e => {
+        e.preventDefault();
+        pagina++;
+        cargarPromos();
+    });
+});
+function cargarPromos(){
     $.post(
         './controller/promociones.php',
         {pagina:pagina},
         msg => {
-            $('#listpromos').html(msg)
+            $('#listpromos').append(msg)
         }
     )
-});
+}
