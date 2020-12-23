32ad3cb7be2512c360d7df28aeadc595ec103111
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-23 04:03:54 -0300

    usar foreach para reiniciar cuenta

--- controller/public/categorias.php
+++ controller/public/categorias.php
@@ -11,12 +11,12 @@
 
     include './view/public/categorias/tituloCategorias.php';
 
-    while ($categoria = $categorias->fetch_object()):
-        $nombre = $categoria->nombre;
-        $fotocate = getFotoCate($tipo, $categoria->id);
+    foreach ($categorias as $categoria):
+        $nombre = $categoria['nombre'];
+        $fotocate = getFotoCate($tipo, $categoria['id']);
         $foto = './img/'.$fotocate->idUsu.'/'.$tipo.'/'.$fotocate->foto ?? './img/proximamente.png';
         include './view/public/categorias/categoria.php';
-    endwhile;
+    endforeach;
 
     include './view/public/categorias/cierreCategorias.html';
 ?>
