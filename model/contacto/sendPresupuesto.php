a6effce2b69ead3891a058f27ee07ddb15983c61
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-22 21:42:28 -0300

    cerrar etiquetas correctamente

--- model/contacto/sendPresupuesto.php
+++ model/contacto/sendPresupuesto.php
@@ -17,11 +17,11 @@
                 <h2 style=\"color: #c1a152;\">Datos del evento</h2>
                 <h3>Fecha</h3><h5>$fecha</h5>
                 <h3>Cantidad</h3><h5>$cantidad</h5>
-                <h2 style=\"color: #c1a152;\">Datos de contacto</h3>
+                <h2 style=\"color: #c1a152;\">Datos de contacto</h2>
                 <h3>Nombre</h3><h5>$nombre $apellido</h5>
                 <h3>Email</h3><h5>$usrmail</h5>
                 <h3>Telefono</h3><h5>$telefono</h5>
-                <h2 style=\"color: #c1a152;\">Comentarios</h3>
+                <h2 style=\"color: #c1a152;\">Comentarios</h2>
                 <div style=\"text-align: -webkit-center;\">
                     <p style=\"text-align: justify; width: 25em;\">$comentarios</p>
                 </div>";
