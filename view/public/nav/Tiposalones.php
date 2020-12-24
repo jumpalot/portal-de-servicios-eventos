1a2d7b25bddacb08823119fa8b87d9f1b53e6134
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-24 02:32:59 -0300

    indentar

--- view/public/nav/Tiposalones.php
+++ view/public/nav/Tiposalones.php
@@ -1,13 +1,13 @@
-<?php
-    $tpsal = getTiposalones();
-?>
+<?php $tpsal = getTiposalones(); ?>
 <li class="nav-item dropdown" id="ddsalon">
-        <a class="nav-link dropdown-toggle" onclick="window.location='?section=categorias&tipo=salon'" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-          Salones
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <?php while($tp = $tpsal->fetch_object()): ?>
-            <a class="dropdown-item" href="?section=search&tipo=salon&subtipo=<?=$tp->id?>"><?=$tp->nombre?></a>
-           <?php endwhile; ?>
-        </div>
-      </li>
+  <a class="nav-link dropdown-toggle" onclick="window.location='?section=categorias&tipo=salon'" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+    Salones
+  </a>
+  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <?php while($tp = $tpsal->fetch_object()): ?>
+      <a class="dropdown-item" href="?section=search&tipo=salon&subtipo=<?=$tp->id?>">
+        <?=$tp->nombre?>
+      </a>
+    <?php endwhile; ?>
+  </div>
+</li>
--- view/public/nav/Tiposervicios.php
+++ view/public/nav/Tiposervicios.php
@@ -1,13 +1,13 @@
-<?php
-    $tpser = getTiposervicios();
-?>
+<?php $tpser = getTiposervicios(); ?>
 <li class="nav-item dropdown" id="ddservicios">
-        <a class="nav-link dropdown-toggle" onclick="window.location='?section=categorias&tipo=servicios'" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-          Servicios
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <?php while($tp = $tpser->fetch_object()): ?>
-            <a class="dropdown-item" href="?section=search&tipo=servicios&subtipo=<?=$tp->id?>"><?=$tp->nombre?></a>
-           <?php endwhile; ?>
-        </div>
-      </li>
+  <a class="nav-link dropdown-toggle" onclick="window.location='?section=categorias&tipo=servicios'" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+    Servicios
+  </a>
+  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    <?php while($tp = $tpser->fetch_object()): ?>
+      <a class="dropdown-item" href="?section=search&tipo=servicios&subtipo=<?=$tp->id?>">
+        <?=$tp->nombre?>
+      </a>
+    <?php endwhile; ?>
+  </div>
+</li>
