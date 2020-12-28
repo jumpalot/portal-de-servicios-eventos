45cd367c4cb2dd5481df6ef19bcfe5c5b535178b
Mimirko <emirorellana18@gmail.com>
2020-12-28 00:47:55 -0300

    segundo intento agregar contacto

--- model/contacto/sendContacto.php
+++ model/contacto/sendContacto.php
@@ -1,10 +1,34 @@
 <?php
     #incluir librerias
-        #include '../email.php';
+        include '../email.php';
     #recibir datos
+    $dest = $_POST['destinatario'];
+    $usrmail = $_POST['email'];
+    $titulo = $_POST['titulo'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $telefono = $_POST['telefono'];
+    $comentarios = $_POST['comentarios'];
+    
+    $asunto = "Motivo del mensaje $titulo";
+    $cuerpo =  "<div style=\"text-align: center;\">
+                <h1>$asunto</h1>
+                <h2 style=\"color: #c1a152;\">Datos de contacto</h2>
+                <h3>Nombre</h3><h4>$nombre $apellido</h4>
+                <h3>Email</h3><h4>$usrmail</h4>
+                <h3>Telefono</h3><h4>$telefono</h4>
+                <h2 style=\"color: #c1a152;\">Comentarios</h2>
+                <div style=\"text-align: -webkit-center;\">
+                    <p style=\"text-align: justify; width: 25em;\">$comentarios</p>
+                </div>";
+
         # $asunto = $_POST['asunto'];
     #enviar email
-        #sendMail($destinatario,$asunto,$cuerpo);
+        if(sendMail($destinatario,$asunto,$cuerpo)){
+            echo '<h4>Mensaje enviado</h4>';
+        }else {
+            echo'<H4>Error en el envio</h4>';
+        }
     #hacer echo de lo que se vera en el modal resultado
-    echo '<h4>Mensaje enviado</h4>';
+
 ?>
