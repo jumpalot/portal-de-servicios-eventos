9a185f48284c1f00ea4e57d9a6ebf7e1c2bebf8a
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-09 15:57:29 -0300

    descuento movido a esquina superior izquierda

--- view/public/busqueda/items/pro.php
+++ view/public/busqueda/items/pro.php
@@ -1,4 +1,7 @@
 <div class="productos br  input-contenedor-pro" id="<?=$tp->id?>-<?=$tipo?>" onclick="iraDetalle(this.id)">
+    <?php if ($tp->descuento!="0"):?>
+        <label class="btlr pd10 "><?=$tp->descuento?>% OFF</label>
+    <?php endif; ?>
      <div class="cont2">
         <p class="cuerpo2">
             <i class="fas fa-map-marker-alt"></i>
@@ -8,9 +11,6 @@
         <p class="cuerpo1 lnh">
             <?=strip_tags($tp->descripcion)?>
         </p>
-        <?php if ($tp->descuento!="0"):?>
-                <label class="btlr pd10 "><?=$tp->descuento?>% OFF</label>
-        <?php endif; ?>
      </div>
     <div class="cont1 pd10 text-center">
         <img src="./img/<?=$tp->idUsu?>/<?=$tipo?>/<?=$tp->foto?>">
