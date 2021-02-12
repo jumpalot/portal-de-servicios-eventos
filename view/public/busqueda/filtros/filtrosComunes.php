d22948a7cdaa1b627256e936a1b28f47f7694121
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-12 17:22:31 -0300

    agregar ids a selects

--- view/public/busqueda/filtros/filtrosComunes.php
+++ view/public/busqueda/filtros/filtrosComunes.php
@@ -1,9 +1,9 @@
-<select class="selectpicker form-control zonas" data-style="input-contenedor" data-width="100%" name="zonas[]" data-live-search="true" title="Ubicación" multiple>
+<select id="filtroZona" class="selectpicker form-control zonas" data-style="input-contenedor" data-width="100%" name="zonas[]" data-live-search="true" title="Ubicación" multiple>
    <?php while($tpzona = $tpszona->fetch_object()):?>
       <option value="<?=$tpzona->id?>"><?=$tpzona->zona?></option>
    <?php endwhile; ?>
 </select>
-<select class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="descuentos[]" data-live-search="false" title="Descuentos" multiple>
+<select id="filtroOferta" class="selectpicker form-control" data-style="input-contenedor" data-width="100%" name="descuentos[]" data-live-search="false" title="Descuentos" multiple>
   <option value="0;51">De 5 a 10%</option>
   <option value="50;101">De 15 a 20%</option>
   <option value="100;201">25% o mas</option>
--- view/public/busqueda/filtros/filtrosSalones.php
+++ view/public/busqueda/filtros/filtrosSalones.php
@@ -1,10 +1,10 @@
-<select class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="subtipos[]" data-live-search="true" title="Tipos de salon" multiple>
+<select id="filtroSubtipo" class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="subtipos[]" data-live-search="true" title="Tipos de salon" multiple>
    <?php while($tpsalones = $tpsSalones->fetch_object()): ?>
       <option value="<?=$tpsalones->id?>"><?=$tpsalones->nombre?></option>
    <?php endwhile; ?>
 </select>
 
-<select class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="cantidades[]" data-live-search="false" title="Capacidad de personas" multiple>
+<select id="filtroCantidad" class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="cantidades[]" data-live-search="false" title="Capacidad de personas" multiple>
   <option value="0;51">Hasta 50 invitados</option>
   <option value="50;101">De 51 a 100</option>
   <option value="100;201">De 101 a 200</option>
