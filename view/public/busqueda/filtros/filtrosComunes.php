af3e003c1bc178d2a01f181ba598521dff8a2447
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-12 00:25:09 -0300

    filtro oferta

--- view/public/busqueda/filtros/filtrosComunes.php
+++ view/public/busqueda/filtros/filtrosComunes.php
@@ -3,3 +3,8 @@
       <option value="<?=$tpzona->id?>"><?=$tpzona->zona?></option>
    <?php endwhile; ?>
 </select>
+<select class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="descuentos[]" data-live-search="false" title="Descuentos" multiple>
+  <option value="0;51">De 5 a 10%</option>
+  <option value="50;101">De 15 a 20%</option>
+  <option value="100;201">25% o mas</option>
+</select>
