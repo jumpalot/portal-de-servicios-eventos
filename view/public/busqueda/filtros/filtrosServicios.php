16a6d1ef8b37f3b81ee08dbac2701f38382861c8
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-12 18:24:27 -0300

    agregar id a filtro

--- view/public/busqueda/filtros/filtrosServicios.php
+++ view/public/busqueda/filtros/filtrosServicios.php
@@ -1,4 +1,4 @@
-<select class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="subtipos[]" data-live-search="true" title="Tipos de servicio" multiple>
+<select id="filtroSubtipo" class="selectpicker form-control subtipo" data-style="input-contenedor" data-width="100%" name="subtipos[]" data-live-search="true" title="Tipos de servicio" multiple>
    <?php while($tpservicios = $tpsServicios->fetch_object()):?>
       <option value="<?=$tpservicios->id?>"><?=$tpservicios->nombre?></option>
    <?php endwhile; ?>
