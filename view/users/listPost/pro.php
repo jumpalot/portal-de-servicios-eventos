d97c749ad5ec7488de7166e6f1e0f2625e0fed09
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-13 17:28:45 -0300

    fix descuento pubs pro en mis publicaciones e indentacion

--- view/users/listPost/pro.php
+++ view/users/listPost/pro.php
@@ -1,21 +1,29 @@
 <div class="productos br  input-contenedor-pro" id="<?=$tipo?>-<?=$pubId?>" onclick="editarPub(this.id)">
-        <div class="cont2 pd10">
-            <p class="cuerpo2">
-                <i class="fas fa-map-marker-alt"></i>&nbsp;<?=$zona?>
-                <br>
-            </p>
-            <h6 id="titulo">☆ <?=$titulo?></h6>
-            <p class="cuerpo1 lnh">
-                <?=strip_tags($desc)?>
-            </p>
-            <?php if ($descu>0):?>
-                <label class="btlr pd10 "><?=$descu?>% OFF</label>
-            <?php endif; ?>
-        </div>
-        <div class="cont1 pd10">
-            <img src="<?=$img?>" width="200" height="200">
-        </div>
-        <div class="fondopro">
-            <img src="<?=$img?>">
-        </div>
+
+    <?php if ($descu>0):?>
+        <label class="btlr pd10 "><?=$descu?>% OFF</label>
+    <?php endif; ?>
+
+    <div class="cont2 pd10">
+
+        <p class="cuerpo2">
+            <i class="fas fa-map-marker-alt"></i>&nbsp;<?=$zona?>
+            <br>
+        </p>
+
+        <h6 id="titulo">☆ <?=$titulo?></h6>
+
+        <p class="cuerpo1 lnh">
+            <?=strip_tags($desc)?>
+        </p>
+        
+    </div>
+
+    <div class="cont1 pd10">
+        <img src="<?=$img?>" width="200" height="200">
+    </div>
+    
+    <div class="fondopro">
+        <img src="<?=$img?>">
+    </div>
 </div>
