79e427cd5061d3318bc15ba8ca81b572ab15bc78
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2021-02-18 16:55:28 -0300

    remover temporalmente filtro por descuento

--- view/public/busqueda/filtros/filtrosComunes.php
+++ view/public/busqueda/filtros/filtrosComunes.php
@@ -3,8 +3,8 @@
       <option value="<?=$tpzona->id?>"<?=($zonaActual==$tpzona->id)?"selected":""?>><?=$tpzona->zona?></option>
    <?php endwhile; ?>
 </select>
-<select id="filtroOferta" class="selectpicker form-control" data-style="input-contenedor" data-width="100%" name="descuentos[]" data-live-search="false" title="Descuentos" multiple>
+<!-- <select id="filtroOferta" class="selectpicker form-control" data-style="input-contenedor" data-width="100%" name="descuentos[]" data-live-search="false" title="Descuentos" multiple>
   <option value="0;51">De 5 a 10%</option>
   <option value="50;101">De 15 a 20%</option>
   <option value="100;201">25% o mas</option>
-</select>
+</select> -->
