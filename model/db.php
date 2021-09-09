e54340542c445c9e9cffcfcc04554b927546fcc0
Mimirko <Emirorellana18@gmail.com>
2021-09-09 20:02:50 -0300

    intento de cambiar de host

--- model/db.php
+++ model/db.php
@@ -578,5 +578,5 @@
         if ($db->error) echo '<script>console.log(`'.$db->error.'`);</script>';
         return $db->error=="";
     }
-    $db = new mysqli('localhost','id17537878_proyectopresentacion','6NCsC0[=P\mH*-7w','id17537878_u812890733_portalgardey');
+    $db = new mysqli('localhost','id17537878_proyectopresentacion','!0o8|%*Lv59oYN+b','id17537878_u812890733_portalgardey');
 ?> 
